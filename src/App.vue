170549f3cf0410caf4136b0bb79020b46d201f24
Antonije Pavlicek <tonytoy@live.com>
2017-08-02 21:42:49 +0200

    Local storage added

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <header>
-      <input type="text" placeholder="Location from" v-model="locFrom" class="pin mr-20">
-      <input type="text" placeholder="Location to" v-model="locTo" class="pin mr-20">
+      <input type="text" placeholder="Location from" v-model="from" class="pin mr-20">
+      <input type="text" placeholder="Location to" v-model="to" class="pin mr-20">
       <button v-on:click="addRoute">Add route</button>
     </header>
     <router-view></router-view>
@@ -14,28 +14,36 @@
     name: 'app',
     data () {
       	return {
-          locFrom: '',
-          locTo: '',
+          from: '',
+          to: '',
           routesList: [],
       	}
     },
     methods: {
       addRoute: function() {
-        if(this.locFrom!=''&&this.locTo!="") {
+        if(this.from!=''&&this.to!="") {
           var obj = {
-            from: this.locFrom,
-            to: this.locTo
+            from: this.from,
+            to: this.to
           }
           this.routesList.push(obj);
-          this.locFrom = '';
-          this.locTo = '';
+          localStorage.setItem("routes", JSON.stringify(this.routesList));
+          this.from = '';
+          this.to = '';
         } else {
 
         }
-      } 
+      },
     },
     created() {
+      //Load html
+      var ls = localStorage.getItem("routes");
+      if(ls!=null)
+        this.routesList = JSON.parse(ls);
       
+      //localStorage.setItem("lastname", "Smith");
+      //
+      //localStorage.removeItem("lastname");
     }
   }
 </script>
--- src/components/Routes.vue
+++ src/components/Routes.vue
@@ -6,11 +6,11 @@
         You haven't added any routes yet.
       </div>
       <table v-show="this.$parent.routesList.length > 0">
-        <tr v-for="route in this.$parent.routesList">
+        <tr v-for="(route,key,index) in this.$parent.routesList">
           <td>{{route.from}}</td>
           <td>{{route.to}}</td>
-          <td width="138"><router-link to="route-detail/"><button class="small fixed-size">View route</button></router-link></td>
-          <td width="35"><i class="icon delete"></i></td>
+          <td width="130"><router-link to="route-detail/"><button class="small fixed-size">View route</button></router-link></td>
+          <td width="45"><i v-on:click="removeRoute(route)" class="icon delete"></i></td>
         </tr>
       </table>
     </div>
@@ -23,7 +23,19 @@
     data () {
       return {
       }
-    }
+    },
+    methods: {
+      removeRoute: function(o) {
+        var l = this.$parent.routesList;
+        for(var i=0;i<l.length;i++) {
+          if(l[i].from==o.from&&l[i].to==o.to) {
+             this.$parent.routesList.splice(i,1);
+             localStorage.setItem("routes", JSON.stringify(this.$parent.routesList));
+            break;
+          }
+        }
+      },
+    },
 }
 </script>
 
